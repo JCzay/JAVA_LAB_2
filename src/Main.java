9acae6a884d6559c5c5569887de58a71955732f8
kmuller86 <89707079+kmuller86@users.noreply.github.com>
2022-01-17 17:12:31 +0100

    ćwiczenie 6.

--- src/Main.java
+++ src/Main.java
@@ -4,26 +4,62 @@ import java.util.*;
 
 public class Main {
 
+    // Zadanie 1
+    // Podpunkt B z zadania pierwszego
+
+    public static void metoda1(ArrayList<String> x) {
+        for (String y : x) {
+            System.out.println(y);
+        }
+        System.out.println("Wielkość listy: " + x.size());
+    }
+
     public static void main(String[] args) {
 
-        /* ćw_6 Listy */
+        // Podpunkt A z zadania pierwszego
 
-        /* zad.1 a) Napisać program który stworzy listę ArrayList<String> (lista tablicowa),
-                Proszę dane wprowadzać z klawiatury (można użyć pętli), lista ma mieć przynajmniej 5
-                elementów i elementami mają być nazwy zwierząt.
-                b) proszę stworzyć metodę która będzie miała pętle forEach i będzie wyświetlała elementy Listy
-                -- podpowiedź: for(String zmienna : List<String>)
-                c) proszę usunąć 2 ostatnie elementy listy i dodać do niej 3 inne elementy,
-                następnie wyświetlić listę i wielkość listy,
-                d) następnie proszę wykonać odwrotne sortowanie i znów użyć metody z punktu b) do wyświetlenia
-                elementów listy
-          */
+        System.out.println("Podaj 5 wybranych przez Ciebie nazw zwierząt: ");
+        ArrayList<String> zwierzeta = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            Scanner scan = new Scanner(System.in);
+            zwierzeta.add(scan.next());
+        }
 
+        System.out.println("Wyświetlenie nazw zwierząt wpisanych z klawiatury: ");
+        metoda1(zwierzeta);
 
-         /* zad.2 Napisać program który stworzy Set<Integer> i wprowadzamy 10 liczb z klawiatury
-                niech liczby się powtarzają, proszę posortować a następnie wyświetlić zawartość set-a w konsoli
-                 a następnie przeiterować i wyświetlić forEach-em (jak wyżej)
-         */
+        // Podpunkt C z zadania pierwszego
+
+        zwierzeta.remove(zwierzeta.size() - 1);
+        zwierzeta.remove(zwierzeta.size() - 1);
+        System.out.println("Wyświetlenie nazw zwierząt po usunięciu dwóch ostatnich wpisanych z klawiatury oraz dodanie trzech innych nazw: ");
+        zwierzeta.add("Małpa");
+        zwierzeta.add("Rekin");
+        zwierzeta.add("Żyrafa");
+        metoda1(zwierzeta);
+
+        // Podupunkt D z zadania pierwszego
+
+        System.out.println("Wyświetlenie odwrotne nazw zwierząt z podpunktu C przy pomocy metody z podpunktu B: ");
+        Collections.sort(zwierzeta, Collections.reverseOrder());
+        metoda1(zwierzeta);
+
+        // Zadanie 2
+
+        System.out.println("Podaj 10 losowych liczb: ");
+        Set<Integer> liczby = new TreeSet<>();
+        for (int i = 0; i < 10; i++) {
+            Scanner scan = new Scanner(System.in);
+            liczby.add(scan.nextInt());
+        }
+
+        // Wyświetlenie przy pomocy foreach
+
+        System.out.println(liczby);
+        for (Integer a : liczby) {
+            System.out.println(a);
+        }
+
 
     }
 
