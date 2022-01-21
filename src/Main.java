26f92589041e3138781901939f879f9c47a21131
JCzay <Julian.Czaja99@icloud.com>
2022-01-21 09:02:21 +0100

    2/2 Drugiego Ćwiczenia

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
 
         /* ćw.2 pętle for */
 
@@ -8,10 +11,36 @@ public class Main {
          *  i wyświetlał liczby które są podzielne przez ostatnią cyfrę waszego indeksu
          * (jesli tą liczbą jest 0 to przez 2 ostatnie) */
 
+        // Ćwiczenie 1
+        // Mój numer indeksu: 57652
+
+        for(int k = 100; k > 0; k--) {
+            if ( k % 2 == 0) {
+                System.out.println(k);
+            }
+
+        }
+        System.out.println("Wszystkie powyższe liczby są podzielne przez ostatni numer mojego indeksu");
 
         /* zad.2 Napisz program który będzie uzupełniał tablicę 5-cio elementową int[] liczbami
          * wprowadzanymi z klawiatury (użyć klasy Scanner), a następnie będzie wyświetlał elementy tablicy
          * za pomocą pętli forEach powiększone o 11 */
+
+        // Ćwiczenie 2
+
+        System.out.println("Podaj 5 wybranych przez Ciebie liczb: ");
+        Scanner scan = new Scanner(System.in);
+        int tab[] = new int[5];
+
+        for (int z = 0; z < 5; z++){
+            tab[z] = scan.nextInt();
+        }
+
+        for (int liczba : tab){
+            System.out.println(liczba + 11);
+        }
+
+        System.out.println("Wszystkie podane przez Ciebie liczby zostały zwiększone dokładnie o 11!");
 
 
     }
