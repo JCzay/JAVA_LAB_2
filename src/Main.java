8fc05254aa1dd4391f3ecba8b22d56e7d159dbea
JCzay <Julian.Czaja99@icloud.com>
2022-01-24 09:36:21 +0100

    ćw. 4

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,42 @@ public class Main {
         c) w trzeciej pętli return
         */
 
+        // Zadanie 1
+        // a)
+        // przedział liczb od 100 do 15
+
+        System.out.println("\nPrzedział liczb od 100 do 15");
+
+        for(int i = 100;i<=100;i--){
+        System.out.println(i);
+            if (i == 15) break;
+        }
+
+        // b)
+
+        System.out.println("\nPrzedział liczb podzielnych przez 3 z przedziału od 0 do 1000");
+
+        for(int i = 0; i<=1000; i++){
+            if (i%3!=0) continue;
+            System.out.println(i);
+        }
+
+        // c)
+
+        System.out.println("\nPrzedział liczb od 0 do 440");
+
+        for(int i = 0;i<=555;i++){
+            System.out.println(i);
+            if (i == 440) return;
+        }
+
+
+
+
+
+
+
+
 
     }
 }
